30e3897d1db910cf3d278425c08ca0ae0edca9ac
Salar Einavi Pour <salareinavipour@gmail.com>
2017-03-23 19:30:33 +0430

    Add category table seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -22,3 +22,18 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Category::class, function (Faker\Generator $faker) {
+    // Creating local faker for our factories.
+    $persianFaker = Faker\Factory::create("fa_IR");
+
+    return [
+        'category' => $persianFaker->realText(10, 1),
+        'created_by' => rand(0, 50),
+        'uploaded_at' => $faker->dateTime($max = 'now'),
+        'uploaded_by' => rand(0, 50),
+        'revisions' => rand(0, 50),
+        'created_at' => $faker->dateTime($max = 'now'),
+        'updated_at' => $faker->dateTime($max = 'now'),
+    ];
+});
