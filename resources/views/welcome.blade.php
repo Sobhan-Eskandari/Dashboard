a4e19da4b732e22fc618769e0e5498549da715eb
sobhan eskandari <serprofessional@gmail.com>
2017-04-20 20:08:37 +0430

    testing Vps

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Testing permission
+                    Testing permission Hi Vps
                 </div>
 
                 <div class="links">
