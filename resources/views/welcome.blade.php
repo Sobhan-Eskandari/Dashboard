be2efbca25859bbe1dd50843667fa28b61474757
Hamid <Hamid.vetr@gmail.com>
2017-04-20 18:57:54 +0430

    testing permission

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Testing permission
                 </div>
 
                 <div class="links">
