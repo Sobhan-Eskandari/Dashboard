a9a0698ab805fa666a8556ab66a4cd0b7da50474
sobhan eskandari <serprofessional@gmail.com>
2017-06-28 23:47:25 +0430

    fixed search close in creat post

--- public/js/public.js
+++ public/js/public.js
@@ -18,6 +18,23 @@ $(document).ready(function () {
 
     });
 
+    $('.flip').click(function () {
+
+        //-----------[ Open and close the menu bu toggling between isopen and isclose class ] ------------
+        if ($(this).hasClass('is-open')){
+
+            $(this).toggleClass('is-open is-close');
+            $(this).closest('.menuDetector').next().find('.panel').slideUp();
+
+        }else if($(this).hasClass('is-close')){
+
+            $(this).toggleClass('is-close is-open');
+            $(this).closest('.menuDetector').next().find('.panel').slideDown();
+
+        }
+
+    });
+
 });
 //////////////////////
 
@@ -27,24 +44,24 @@ $(".searchFormListGroup").hide();
 
 $(document).ready(function () {
     $('.iconSearch-1').click(function () {
-        $(".categoryTitle").hide();
-        $(".caretIcon").hide();
-        $(".iconSearch-1").hide();
-        $(".searchFormListGroup").show();
-        $(".iconSearch-2").show();
-        $(".iconClose-2").show();
+        $(this).parent().find(".categoryTitle").hide();
+        $(this).parent().find(".caretIcon").hide();
+        $(this).parent().find(".iconSearch-1").hide();
+        $(this).parent().find(".searchFormListGroup").show();
+        $(this).parent().find(".iconSearch-2").show();
+        $(this).parent().find(".iconClose-2").show();
     });
 });
 
 $(document).ready(function () {
     $('.iconClose-2').click(function () {
-        $(".categoryTitle").show();
-        $(".caretIcon").show();
-        $(".iconSearch-1").show();
-        $(".iconDelete-1").show();
-        $(".searchFormListGroup").hide();
-        $(".iconSearch-2").hide();
-        $(".iconClose-2").hide();
+        $(this).parent().parent().parent().find(".categoryTitle").show();
+        $(this).parent().parent().parent().find(".caretIcon").show();
+        $(this).parent().parent().parent().find(".iconSearch-1").show();
+        $(this).parent().parent().find(".iconDelete-1").show();
+        $(this).parent().parent().find(".searchFormListGroup").hide();
+        $(this).parent().parent().find(".iconSearch-2").hide();
+        $(this).parent().parent().find(".iconClose-2").hide();
     });
 
 
