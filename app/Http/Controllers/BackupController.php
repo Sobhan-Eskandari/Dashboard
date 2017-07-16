e6a8392db6fe7e8aed506db25a294f1bd3c4d374
Nima <nima.shirinzadeh@gmail.com>
2017-07-16 19:30:31 +0430

    backups are done

--- app/Http/Controllers/BackupController.php
+++ app/Http/Controllers/BackupController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Admin;
+use App\Comment;
 use App\Inbox;
+use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Maatwebsite\Excel\Facades\Excel;
@@ -21,7 +25,17 @@ class BackupController extends Controller
 
     public function posts(Request $request)
     {
-        //
+        $input = $request->all();
+        $from = str_replace('/', '-', $input['from']) . ' 00:00:00';
+        $till = str_replace('/', '-', $input['till']) . ' 23:59:59';
+        $posts = Post::whereBetween('created_at', [$from, $till])->get()->toArray();
+
+        return Excel::create('All_Post', function($excel) use ($posts) {
+            $excel->sheet('mySheet', function($sheet) use ($posts)
+            {
+                $sheet->fromArray($posts);
+            });
+        })->export($input['type']);
     }
 
     public function inboxes(Request $request)
@@ -41,16 +55,47 @@ class BackupController extends Controller
 
     public function users(Request $request)
     {
-        //
+        $input = $request->all();
+        $from = str_replace('/', '-', $input['from']) . ' 00:00:00';
+        $till = str_replace('/', '-', $input['till']) . ' 23:59:59';
+        $users = User::whereBetween('created_at', [$from, $till])->get()->toArray();
+
+        return Excel::create('All_Users', function($excel) use ($users) {
+            $excel->sheet('mySheet', function($sheet) use ($users)
+            {
+                $sheet->fromArray($users);
+            });
+        })->export($input['type']);
     }
 
     public function comments(Request $request)
     {
-        //
+        $input = $request->all();
+        $from = str_replace('/', '-', $input['from']) . ' 00:00:00';
+        $till = str_replace('/', '-', $input['till']) . ' 23:59:59';
+        $comments = Comment::whereBetween('created_at', [$from, $till])->get()->toArray();
+
+        return Excel::create('All_comments', function($excel) use ($comments) {
+            $excel->sheet('mySheet', function($sheet) use ($comments)
+            {
+                $sheet->fromArray($comments);
+            });
+        })->export($input['type']);
+
     }
 
     public function admins(Request $request)
     {
-        //
+        $input = $request->all();
+        $from = str_replace('/', '-', $input['from']) . ' 00:00:00';
+        $till = str_replace('/', '-', $input['till']) . ' 23:59:59';
+        $admins = Admin::whereBetween('created_at', [$from, $till])->get()->toArray();
+
+        return Excel::create('All_Admins', function($excel) use ($admins) {
+            $excel->sheet('mySheet', function($sheet) use ($admins)
+            {
+                $sheet->fromArray($admins);
+            });
+        })->export($input['type']);
     }
 }
