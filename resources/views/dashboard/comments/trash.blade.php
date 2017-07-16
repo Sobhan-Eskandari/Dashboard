16019c5eb77d1d4609d15ae8230605ee9d3d8d2d
Nima <nima.shirinzadeh@gmail.com>
2017-07-16 17:57:06 +0430

    Users

--- resources/views/dashboard/comments/trash.blade.php
+++ resources/views/dashboard/comments/trash.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.main')
-@section('search')
-    <div class="hi-search-1">
-        {!! Form::open(['method'=>'GET', 'action'=>'CommentController@trash']) !!}
-        {!! Form::text('query', isset($_GET['query'])? $_GET['query'] : '', ['class' => 'hi-search_field', 'placeholder'=>'جست و جو کنید...', 'id'=>'searchCmt']) !!}
-        <button class="hi-button-btn1 pull-left" id="cmtSearch"><i class="fa fa-search white-text hi-fontSize-19" aria-hidden="true"></i></button>
-        {!! Form::close() !!}
-    </div>
-@endsection
+{{--@section('search')--}}
+    {{--<div class="hi-search-1">--}}
+        {{--{!! Form::open(['method'=>'GET', 'action'=>'CommentController@trash']) !!}--}}
+        {{--{!! Form::text('query', isset($_GET['query'])? $_GET['query'] : '', ['class' => 'hi-search_field', 'placeholder'=>'جست و جو کنید...', 'id'=>'searchCmt']) !!}--}}
+        {{--<button class="hi-button-btn1 pull-left" id="cmtSearch"><i class="fa fa-search white-text hi-fontSize-19" aria-hidden="true"></i></button>--}}
+        {{--{!! Form::close() !!}--}}
+    {{--</div>--}}
+{{--@endsection--}}
 @section('breadcrumb')
     @component('components.Breadcrumb')
 
